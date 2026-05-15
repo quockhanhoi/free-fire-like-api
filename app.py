09758a1464ccf12b23d7675a0a61b59be0e63c2c
quockhanhoi <quockhanh.dalat13@gmail.com>
2026-05-15 17:40:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -83,12 +83,16 @@ def like():
         
        
     })
+    from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Server đang hoạt động!"
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
-
-
-
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
 
 
 
